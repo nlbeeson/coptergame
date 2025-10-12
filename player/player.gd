5fbb218a7b3999d4b22e290f2af803d907a0dfbd
nlbeeson <emtnate@gmail.com>
2025-10-12 17:26:39 -0400

    Added simple capsule as player. Added rotation with y movement.

--- player/player.gd
+++ player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed = 10000
 @export var gravity = 1000
 @export var thrust_force = 400
+@export var fuel = 100.0
 
 
 func _physics_process(delta):
@@ -10,11 +11,23 @@ func _physics_process(delta):
 		velocity.y += gravity * delta # simple gravity
 		
 		velocity.x = speed * delta # simple movement to the right
+	else:
+		velocity.x = 0
 		
 	if Input.is_action_just_pressed("thrust"):
 		velocity.y = -thrust_force
-	if Input.is_action_pressed("left"): # apply force to the left when action is held
-		velocity.x -= thrust_force
-	if Input.is_action_pressed("right"): # apply force to the right when action is held
-		velocity.x += thrust_force
+		use_fuel(1)
+	# Todo: Add left / right thrust movement
+		
+	# Rotate sprite with direction
+	# Todo: smooth the rotation transition with slerp function
+	if velocity.y < 0:
+		rotation_degrees = -20
+	if velocity.y > 0:
+		rotation_degrees = 45         
+	if velocity.y == 0:
+		rotation_degrees = 0
 	move_and_slide()
+
+func use_fuel(fuel_used):
+	fuel -= fuel_used
