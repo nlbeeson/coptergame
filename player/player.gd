05ca1f7716345fb854ff00099c966e60ab6f2219
nlbeeson <emtnate@gmail.com>
2025-11-27 22:54:12 -0500

    Updated player controller
    Updated player controller to add left / right thrust and braking with
    continued right scrolling.

--- player/player.gd
+++ player/player.gd
@@ -1,34 +1,62 @@
 extends CharacterBody2D
 
-@export var speed = 10000
 @export var gravity = 1000
-@export var thrust_force = 400
+@export var thrust_force = 2500
+@export var side_thrust_force = 600
+@export var auto_advance_speed = 200 # Base speed the player moves right
+@export var max_relative_speed = 400 # Max extra speed from thrust
 @export var fuel = 100.0
+@export var fuel_consumption_rate = 10.0
 
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta # simple gravity
+	# Apply Gravity
+	velocity.y += gravity * delta
+
+	# Handle Input & Thrust
+	var is_thrusting = false
+	
+	# Vertical Thrust
+	if Input.is_action_pressed("thrust") and fuel > 0:
+		velocity.y -= thrust_force * delta
+		is_thrusting = true
+	
+	# Horizontal Movement
+	var side_input = Input.get_axis("left", "right")
+	if side_input != 0 and fuel > 0:
+		velocity.x += side_input * side_thrust_force * delta
+		is_thrusting = true
 		
-		velocity.x = speed * delta # simple movement to the right
+
+	if is_thrusting:
+		use_fuel(fuel_consumption_rate * delta)
+
+	# Passive forward acceleration to maintain pace
+	if velocity.x < auto_advance_speed:
+		velocity.x += 100 * delta # Slow acceleration to base speed
+		
+	# Rotation / Visuals
+	
+	# Flip sprite based on input
+	#if side_input < 0:
+	#	$Sprite.flip_h = true
+	#elif side_input > 0:
+	#	$Sprite.flip_h = false
+	
+	# Tilt based on horizontal velocity relative to base speed
+	var target_rotation = 0.0
+	if velocity.x > auto_advance_speed + 50:
+		target_rotation = 15.0 # Tilt forward
+	elif velocity.x < auto_advance_speed - 50:
+		target_rotation = -10.0 # Tilt back
 	else:
-		velocity.x = 0
+		target_rotation = 0.0
 		
-	if Input.is_action_just_pressed("thrust") and fuel >= 0:
-		velocity.y = - thrust_force
-		use_fuel(1)
-	# Todo: Add left / right thrust movement
-		
-	# Rotate sprite with direction
-	# Todo: smooth the rotation transition with slerp function
-	if velocity.y < 0:
-		rotation_degrees = -20
-	if velocity.y > 0:
-		rotation_degrees = 45
-	if velocity.y == 0:
-		rotation_degrees = 0
+	# Smooth rotation
+	rotation_degrees = lerp(rotation_degrees, target_rotation, 5 * delta)
+
 	move_and_slide()
 
-func use_fuel(fuel_used):
-	fuel -= fuel_used
-	print("Fuel: " + str(fuel))
+func use_fuel(amount):
+	fuel -= amount
+	# print("Fuel: " + str(fuel)) # Debug
